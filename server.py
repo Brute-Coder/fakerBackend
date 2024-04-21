0c74f2aa8727b3d5523b6cb3ee3dd8d5d5408fcd
Soumya Sekhar Mahanti <soumyasekharmahanti@gmail.com>
2024-04-21 19:07:48 +0530

    added netLogger

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from generators.SQL_server_security import sql_event_generator
 from generators.windows_event import generate_windows_events
 from generators.suricata_events import generate_suricata_events
 from generators.active_directory import directory_events_loop
+from generators.sflow import generate_sFlow
 from storageHandler import store_event_in_file
 
 app = Flask(__name__)
@@ -15,7 +16,8 @@ defaultFiles = {
     "sql-server" : "sql_server.txt",
     "windows_gen":"windows_events.txt",
     "suricata_event" : "suricata_events.txt",
-    "directory_events": "directory_events.txt"
+    "directory_events": "directory_events.txt",
+    "sFlow_events" : "sFlow_events.txt"
 }
 
 @app.route('/system-process', methods=['GET'])
@@ -28,7 +30,6 @@ def generate_events():
         events.append(event)
     if saveLog:
         file_name = request.args.get('filename')
-        # print(file_name)
         if not file_name:
             file_name = defaultFiles['system-process']
         store_event_in_file(file_name = file_name, event_data = events)
@@ -81,7 +82,20 @@ def generate_directory_events():
             file_name = defaultFiles['directory_events']
         store_event_in_file(file_name = file_name,event_data = events)
     return jsonify(events)
-    # return events
+
+@app.route('/sFlow-events',methods = ['GET'])
+def sFlow():
+    eventQty = int(request.args.get('qty'))
+    saveLog = bool(int(request.args.get('savelog')))
+    events = directory_events_loop(eventQty)
+    if saveLog:
+        file_name = request.args.get('filename')
+        if not file_name:
+            file_name = defaultFiles['sFlow_events']
+        store_event_in_file(file_name = file_name,event_data = events)
+    return jsonify(events)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
